3e5405f16a55f7436a9f47b3c1a60ec741636d60
diogolhc <64315652+diogolhc@users.noreply.github.com>
2022-01-06 20:09:44 +0000

    Reorganize files

---
+++ src/bot.pl
@@ -0,0 +1,5 @@
+% value(+GameState, +Player, -Value)
+% TODO (note: GameState is not only the Board)
+
+% choose_move(+GameState, +Level, -Move)
+% TODO (note: GameState is not only the Board)
--- src/logic.pl
+++ src/logic.pl
@@ -6,9 +6,3 @@
 
 % valid_moves(+GameState, -ListOfMoves)
 % TODO (note: GameState is not only the Board)
-
-% value(+GameState, +Player, -Value)
-% TODO (note: GameState is not only the Board)
-
-% choose_move(+GameState, +Level, -Move)
-% TODO (note: GameState is not only the Board)
