bb45e2bbfb953054c274387196a17173daddb582
up201905405 <up201905405@fe.up.pt>
2022-01-15 16:29:46 +0000

    pc.pl documentation

--- src/pc.pl
+++ src/pc.pl
@@ -1,5 +1,9 @@
 :- use_module(library(random)).
 
+/*
+    Converts a game piece (tank) into its evaluation value to be 
+    used in the board evaluation
+*/
 % tank_value_map(+Tank, -Value)
 tank_value_map(0, 0).
 tank_value_map(1, 100).
@@ -11,15 +15,25 @@ tank_value_map(Negative, Val):-
     tank_value_map(Positive, V1),
     Val is -1*V1.
 
-
+/*
+    Each tank in a list is replaced by its evaluation value
+*/
 % apply_tank_value_map(+TankList, -ValueList)
 apply_tank_value_map([], []).
-
 apply_tank_value_map([Tank | RestTanks], [Value | RestValues]):-
     tank_value_map(Tank, Value),
     apply_tank_value_map(RestTanks, RestValues).
 
-
+/* 
+    Evaluates a line of the board for either of the players
+    First the enemy tanks of Player are filtered from the line,
+    then all the tanks are replaced by their evaluation value
+    and get multiplied by the distance factor, this distance factor
+    is 2^n where n is the number of lines between the current line
+    being evaluated and the line closest to the Player. After these
+    opperations the value of the line is the sum of the resultant
+    list
+*/
 % evaluate_line(+Board, +Line, +Player, -Value)
 evaluate_line(Board, Line, bot, Value):-
     length(Board, Size),
@@ -30,7 +44,6 @@ evaluate_line(Board, Line, bot, Value):-
     Multiplier is 2 ** DistanceCovered,
     list_multiply_scalar(Multiplier, ValueList, ScaledValues),
     sum_list(ScaledValues, Value).
-
 evaluate_line(Board, Line, top, Value):-
     nth0(Line, Board, LineList),
     list_multiply_scalar(-1, LineList, NormalizedList),
@@ -41,10 +54,13 @@ evaluate_line(Board, Line, top, Value):-
     list_multiply_scalar(Multiplier, ValueList, ScaledValues),
     sum_list(ScaledValues, Value).
 
-
+/*
+    Evaluates all the lines of the board for a Player and
+    adds them resulting in the evaluation of the board
+    for a Player
+*/
 % evaluate_lines(+Board, +Size, +Player, -Value)
 evaluate_lines(_Board, 0, _Player, 0).
-
 evaluate_lines(Board, Size, Player, Value):-  
     SizeLeft is Size - 1,
     Size > 0,
@@ -52,7 +68,12 @@ evaluate_lines(Board, Size, Player, Value):-
     evaluate_lines(Board, SizeLeft, Player, ValueAcc),
     Value is ValueAcc+CurrLineValue.
 
-
+/*
+    Evaluates the current GameSate for a Player, the board
+    is evaluated for both Players and result is the subtraction
+    of the evaluation of the Board for the Given player by the
+    evaluation of the Board of its opponent
+*/
 % param +Player is either top or bot (bot = bottom, not PC)
 % value(+GameState, +Player, -Value)
 value(_-Board, top, Value):-
@@ -64,7 +85,11 @@ value(GameState, bot, Value):-
     value(GameState, top, Inverted),
     Value is -1*Inverted.
 
-
+/*
+    Compares two valid moves deciding which is the one that results
+    in the best value. In case of a tie of them is randomly selected
+    with 1/2 chance
+*/
 % best_move(+Move1, +Value1, +Move2, +Value2, -BestMove, -BestValue)
 best_move(Move1, Value1, _Move2, Value2, Move1, Value1):-
     Value1 > Value2.
@@ -75,7 +100,12 @@ best_move(Move1, Value12, Move2, Value12, Move3, Value3):-
     matrix_at(MoveValueList, [1, Res], Value3).
 best_move(_Move1, _Value1, Move2, Value12, Move2, Value12).
 
-
+/*
+    From a List of valid moves that can be done by a player, it
+    simulates each of them in a copy of the board and then evaluates 
+    these modified GameStates selecting the one that has the highest
+    evaluation
+*/
 % highest_value_move(+GameState, +MoveList, -BestValue, -BestMove)
 highest_value_move(Turn-Board, [LastMove], LastMoveValue, LastMove):- 
     do_valid_move(Board, LastMove, ResultBoard),
@@ -86,7 +116,12 @@ highest_value_move(Turn-Board, [CurrentMove | RestMoves], BestValue, BestMove):-
     highest_value_move(Turn-Board, RestMoves, NextValue, NextMove),
     best_move(CurrentMove, CurrentValue, NextMove, NextValue, BestMove, BestValue).
 
-
+/*
+    Chooses a move to be done by the pc player while taking into
+    account its level, if it's level 1 it selects a random move,
+    if it's level 2 it selects the best move possible according
+    to the evaluation method being used
+*/
 % choose_move(+GameState, +Level, -Move)
 choose_move(GameState, 2, Move):- 
     valid_moves(GameState, ValidMoves),
